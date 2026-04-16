25bad22fd91ec2cccfac99abaffdf0855ebc76a4
Maxim Demin <deminmaxim14@gmail.com>
2026-04-16 22:29:01 +0300

    Add backend docker

--- backend/kittygram_backend/settings.py
+++ backend/kittygram_backend/settings.py
@@ -8,7 +8,13 @@ SECRET_KEY = 'django-insecure-cg6*%6d51ef8f#4!r3*$vmxm4)abgjw8mo!4y-q*uq1!4$-89$
 
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [
+    'localhost',
+    '127.0.0.1',
+    '123.123.123.123',
+    'kittybot.ddns.net',
+    '158.160.237.7'
+]
 
 INSTALLED_APPS = [
     'django.contrib.admin',
